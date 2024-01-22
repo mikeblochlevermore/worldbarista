d46d6b955beccaceaa0097f2d04d3e5f0b2e7a4f
bjoexxxx <bjoe6702@stud.kea.dk>
2024-01-22 20:54:37 +0100

    somewhat successful imitation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,56 @@
 window.onload = function () {
 
-    var el = document.createElement("div");
-    el.innerHTML = "RABBIT TEST ";
+    var container = document.createElement("div");
+    container.className = 'image-container';
+
+    var image = document.createElement("img");
+    image.className = 'headline-image';
+    var text = document.createElement("div");
+    text.className = 'overlay-text';
+
+    var imageCount = 1;
+
+    var nextButton = document.createElement("button");
+    nextButton.innerHTML = "Next";
+    nextButton.onclick = function() {
+        imageCount = (imageCount % 3) + 1;
+        selectImage();
+    };
+    nextButton.className = 'button';
+
+    var previousButton = document.createElement("button");
+    previousButton.innerHTML = "Previous";
+    previousButton.onclick = function() {
+        imageCount = ((imageCount + 1) % 3) + 1;
+        selectImage();
+    };
+    previousButton.className = 'button';
+
+    container.appendChild(image);
+    container.appendChild(text);
+    container.appendChild(previousButton);
+    container.appendChild(nextButton);
+
     var div = document.getElementById("headline");
-    insertAfter(div, el);
+    selectImage();
+    insertAfter(div, container);
 
     /* FUNCTIONS */
+    function selectImage() {
+        if (imageCount === 1) {
+            image.src = "assets/images/instagram-feed/instagram-image-5.png";
+            image.alt = "headline-image-1";
+            text.innerHTML = "To create an environment in which knowledge about coffee and its sphere can be obtained";
+        } else if (imageCount === 2) {
+            image.src = "assets/images/instagram-feed/instagram-image-3.png";
+            image.alt = "headline-image-2";
+            text.innerHTML = "This is a great introduction to the coffee industry's best beans on the planet";
+        } else if (imageCount === 3) {
+            image.src = "assets/images/instagram-feed/instagram-image-4.png";
+            image.alt = "headline-image-3";
+            text.innerHTML = "Coffee in, code out, this is the way of eternal life and empowerment of the soul";
+        }
+    }
 
     // Inserts newNode after referenceNode
     function insertAfter(referenceNode, newNode) {
